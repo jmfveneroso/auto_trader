b4cd74e36fb09d9921b80b1c73c14bb569fa5add
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2017-11-30 15:46:03 -0200

    Pretty graphs.

--- gui/gui.js
+++ gui/gui.js
@@ -1,6 +1,6 @@
 (function () {
 
-width = 600;
+width = 1000;
 height = 400;
 margin_x = 68;
 margin_y = 80;
@@ -26,26 +26,44 @@ function date_to_simple_str(d) {
          two(d.getHours()) + ':' + two(d.getMinutes())]
 }
 
-function draw_shadows(ctx, x, min, max, candle_size) {
+function draw_shadows(ctx, x, min, max, candle_size, skip_size) {
   ctx.beginPath();
-  ctx.moveTo(margin_x + x * (candle_size + 4) + candle_size / 2, margin_y + height - max);
-  ctx.lineTo(margin_x + x * (candle_size + 4) + candle_size / 2, margin_y + height - min);
+  ctx.strokeStyle = "#aaaaaa";
+  ctx.lineWidth = 1;
+  ctx.moveTo(margin_x + x * (candle_size + skip_size) + candle_size / 2, margin_y + height - max);
+  ctx.lineTo(margin_x + x * (candle_size + skip_size) + candle_size / 2, margin_y + height - min);
   ctx.stroke();
+  ctx.strokeStyle = "#000000";
   ctx.closePath();
 }
 
-function draw_candle(ctx, x, open, close, candle_size) {
+function draw_date_scale(ctx, x, candle_size, skip_size, date) {
+  arr = date_to_simple_str(date);
+  ctx.fillStyle = "#000000";
+  ctx.fillText(arr[0], margin_x + x * (candle_size + skip_size) - 12, margin_y + height + 30);
+  ctx.fillText(arr[1], margin_x + x * (candle_size + skip_size) - 12, margin_y + height + 42);
+
+  ctx.strokeStyle = "#eeeeee";
+  ctx.lineWidth = 1;
+  ctx.moveTo(margin_x + x * (candle_size + skip_size), margin_y);
+  ctx.lineTo(margin_x + x * (candle_size + skip_size), margin_y + height);
+  ctx.stroke();
+}
+
+function draw_candle(ctx, x, open, close, candle_size, skip_size) {
   length = Math.abs(open - close);
   if (length < 2) length = 2;
   y = (open > close) ? open : close;
 
   ctx.beginPath();
+
   if (open < close)
     ctx.fillStyle = "#00FF00";
   else 
     ctx.fillStyle = "#FF0000";
 
-  ctx.fillRect(margin_x + x * (candle_size + 4), height - y + margin_y, candle_size, length);
+  ctx.fillRect(margin_x + x * (candle_size + skip_size), height - y + margin_y, candle_size, length);
+
   ctx.closePath();
 }
 
@@ -64,7 +82,7 @@ function minutes_between(date1, date2) {
   return Math.round(difference_ms/one_minute); 
 }
 
-function draw_scale(ctx, min, max, min_date, max_date) {
+function draw_scale(ctx, min, max, min_date, max_date, candles) {
   ctx.beginPath();
   ctx.fillStyle = "#000000";
 
@@ -74,20 +92,11 @@ function draw_scale(ctx, min, max, min_date, max_date) {
     ctx.strokeStyle = "#eeeeee";
     ctx.lineWidth = 1;
     ctx.moveTo(margin_x - 10, margin_y + 40 * i);
-    ctx.lineTo(margin_x + 610, margin_y + 40 * i);
+    ctx.lineTo(margin_x + width + 10, margin_y + 40 * i);
     ctx.stroke();
 
-    ctx.fillText(String(value.toFixed(4)), margin_x + 620, margin_y + 40 * i + 2);
+    ctx.fillText(String(value.toFixed(4)), margin_x + width + 20, margin_y + 40 * i + 2);
     value -= step;
-  }
-
-  d = min_date;
-  step = minutes_between(min_date, max_date) / 4;
-  for (var i = 0; i < 5; i ++) {
-    arr = date_to_simple_str(d);
-    ctx.fillText(arr[0], margin_x + i * 143 + 4, margin_y + 430);
-    ctx.fillText(arr[1], margin_x + i * 143 - 1 + 4, margin_y + 442);
-    d = new Date(d.getTime() + step * 60000)
   }
 
   ctx.closePath();
@@ -128,21 +137,36 @@ function plot_candlesticks(new_candles, currency) {
     if (parseFloat(data[i]['max']) > max) max = parseFloat(data[i]['max'])
     if (parseFloat(data[i]['min']) < min) min = parseFloat(data[i]['min'])
   }
-  draw_scale(ctx, min, max, new Date(data[0]['date']), new Date(data[data.length - 1]['date']));
+  draw_scale(ctx, min, max, new Date(data[0]['date']), new Date(data[data.length - 1]['date']), data);
 
-  candle_size = parseInt(600 / data.length) - 4
+  skip_size = 1;
+  candle_size = parseInt(((600 - (data.length - 1)) * skip_size) / data.length);
+  candle_size = (candle_size > 0) ? candle_size : 1;
   for (var i = 0; i < data.length; i++) {
     min_ = ((parseFloat(data[i]['min']) - min) / (max - min)) * height
     max_ = ((parseFloat(data[i]['max']) - min) / (max - min)) * height
-    draw_shadows(ctx, i, min_, max_, candle_size);
+    draw_shadows(ctx, i, min_, max_, candle_size, skip_size);
   }
 
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i])
+  last_date = null;
+  var i = 0;
+  for (; i < data.length; i++) {
     open  = ((parseFloat(data[i]['open']) - min) / (max - min)) * height 
     close = ((parseFloat(data[i]['close']) - min) / (max - min)) * height 
-    draw_candle(ctx, i, open, close, candle_size);
+    draw_candle(ctx, i, open, close, candle_size, skip_size);
+    if (i % 30 == 0)
+      draw_date_scale(ctx, i, candle_size, skip_size, new Date(data[i]['date']));
+    last_date = new Date(data[i]['date']);
   }
+
+  remaining = (30 - (i % 30));
+  console.log(i);
+  i = i + remaining;
+  console.log(last_date);
+  console.log(remaining);
+  last_date = new Date(last_date.getTime() + 15 * 60 * 1000 * remaining);
+  draw_date_scale(ctx, i, candle_size, skip_size, last_date);
+
 }
 
 function update_plots() {
@@ -153,16 +177,20 @@ function update_plots() {
 
 function update_candlesticks() {
   $.get("/update/" + date_to_str(last_update)).done(function (data) {
-    last_update = new Date();
+    // Have to add 2 hours because of time zones.
+    last_update = new Date((new Date()).getTime() + 2 * 60 * 60 * 1000);
 
     new_candles = JSON.parse(data);
     new_candles = new_candles.sort(function (x, y) {
       return (new Date(x['date']).getTime()) - (new Date(y['date'])).getTime();
     });
 
+    if (new_candles.length == 0) return;
+
     currencies = [
-      "btc", "eth", "bch", "ltc", "xrp", "zec", 
-      "etc", "str", "dash", "nxt", "xmr", "rep"
+    //   "btc", "eth", "bch", "ltc", "xrp", "zec", 
+    //   "etc", "str", "dash", "nxt", "xmr", "rep"
+    "btc"
     ];
 
     for (var i = 0; i < currencies.length; i++) {
